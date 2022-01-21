5baf6c5373741f3d8fce2344a8da87f3b53b4361
Diego Suarez <dsuarez@stratio.com>
2022-01-20 20:30:52 -0500

    feat: add project to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  host: 'https://platzi-store.herokuapp.com'
+  host: 'https://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyB8--rSsNTwNPck5mV8MRHO1lqRxRwWg5A',
+    authDomain: 'platzi-curso-angular.firebaseapp.com',
+    projectId: 'platzi-curso-angular',
+    storageBucket: 'platzi-curso-angular.appspot.com',
+    messagingSenderId: '768023296507',
+    appId: '1:768023296507:web:97a9b4dda3d4977751f35e'
+  }
 };
